47f2fed83742dedbd62445088254bc52f586745a
estherviolin <esthernaomi.violin@gmail.com>
2020-12-07 15:11:40 -0600

    user can like, unlike, and view liked products

--- bangazonapi/models/userlike.py
+++ bangazonapi/models/userlike.py
@@ -4,7 +4,7 @@ from .customer import Customer
 
 class UserLike(models.Model):
 
-    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="liked")
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="userlikes")
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     
 
